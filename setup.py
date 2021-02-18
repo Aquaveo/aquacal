0876ffa504b5618b2f2143502e6eda5713d3305f
nswain <nswain@aquaveo.com>
2021-02-18 10:55:03 -0700

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+requirements = [
+    'requests'
+]
+
 setup(
     name='aquacal',
     version='1.0.0',
@@ -8,4 +12,5 @@ setup(
     author_email='nswain@aquaveo.com',
     url='https://www.aquaveo.com',
     packages=find_packages(exclude='tests'),
+    requires=requirements
 )
