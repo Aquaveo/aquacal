c15c64657b75fa9de77b70a6428f186db6b70329
nswain <nswain@aquaveo.com>
2021-03-17 14:37:02 -0600

    Add class with properties to demonstrate property mock.

--- aquacal/utils.py
+++ aquacal/utils.py
@@ -13,3 +13,26 @@ def get_holidays():
     if r.status_code == 200:
         return r.json()
     return None
+
+
+class AquaEvent:
+    def __init__(self, name, date):
+        self._name = name
+        self.date = date
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, val):
+        if not isinstance(val, datetime):
+            raise ValueError("The date property must be a valid datetime object.")
+        self._date = val
+
+    def get_summary(self):
+        return f'{self.name}: {self.date.strftime("%A, %B %D, %Y @ %I:%M %p")}'
